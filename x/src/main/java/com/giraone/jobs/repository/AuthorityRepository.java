1882eca207b124a7cd70ebf9b807d185ad55b975
Hans Schuell <hans.schuell@gmx.de>
2022-12-17 11:16:02 +0100

    Initial version of jobadmin generated by generator-jhipster@7.9.3

---
+++ x/src/main/java/com/giraone/jobs/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.giraone.jobs.repository;
+
+import com.giraone.jobs.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ x/src/test/java/com/giraone/jobs/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.giraone.jobs;
+
+import com.giraone.jobs.JobadminApp;
+import com.giraone.jobs.config.AsyncSyncConfiguration;
+import com.giraone.jobs.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { JobadminApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
