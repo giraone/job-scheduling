881d91f035a6c96a304393e1f957896b9578ce0b
Hans Schuell <hans.schuell@gmx.de>
2023-01-03 07:15:24 +0100

    dockerize script improved

--- docker/dockerize.sh
+++ docker/dockerize.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
+docker stop receiver
 docker rm receiver
 docker rmi receiver
 cd ../receiver
-mvn clean
 mvn -ntp verify -DskipTests jib:dockerBuild
 cd -
 
+docker stop materialize
 docker rm materialize
 docker rmi materialize
 cd ../materialize
-mvn clean
 mvn -ntp verify -DskipTests jib:dockerBuild
 cd -
 
+docker stop schedule
 docker rm schedule
 docker rmi schedule
 cd ../schedule
-mvn clean
 mvn -ntp verify -DskipTests jib:dockerBuild
 cd -
 
+docker stop jobadmin
+docker rm jobadmin
 docker rmi jobadmin
 cd ../jobadmin
 mvn -ntp verify -DskipTests -Pprod jib:dockerBuild
