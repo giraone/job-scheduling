8fb54dae95b53ed695fd822aa666ad27ea59dff3
Hans Schuell <hans.schuell@gmx.de>
2023-01-11 22:34:29 +0100

    Missing import fixed

--- jobadmin/src/main/java/com/giraone/jobs/web/rest/JobRecordResource.java
+++ jobadmin/src/main/java/com/giraone/jobs/web/rest/JobRecordResource.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import com.github.f4b6a3.tsid.Tsid;
+import com.github.f4b6a3.tsid.TsidCreator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springdoc.api.annotations.ParameterObject;
