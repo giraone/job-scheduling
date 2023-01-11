b0b5d9a562f59f271f129636266d323bb58eccd0
Hans Schuell <hans.schuell@gmx.de>
2023-01-11 23:46:50 +0100

    telemetry data added

--- docker/data-cleanup.sh
+++ docker/data-cleanup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-rm -rf ${CONTAINER_DATA:-./data}/control-center
 rm -rf ${CONTAINER_DATA:-./data}/kafka-1
 rm -rf ${CONTAINER_DATA:-./data}/zk-1
+
+rm -rf ${CONTAINER_DATA:-./data}/control-center
+
+rm -rf ${CONTAINER_DATA:-./data}/prometheus
+rm -rf ${CONTAINER_DATA:-./data}/grafana
+rm -rf ${CONTAINER_DATA:-./data}/loki
+rm -rf ${CONTAINER_DATA:-./data}/tempo
--- docker/data-setup.sh
+++ docker/data-setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-mkdir -m 777 ${CONTAINER_DATA:-./data}/control-center
 mkdir -m 777 ${CONTAINER_DATA:-./data}/kafka-1
 mkdir -m 777 ${CONTAINER_DATA:-./data}/zk-1
 mkdir -m 777 ${CONTAINER_DATA:-./data}/zk-1/data
 mkdir -m 777 ${CONTAINER_DATA:-./data}/zk-1/log
+
+mkdir -m 777 ${CONTAINER_DATA:-./data}/control-center
+
+mkdir -m 777 ${CONTAINER_DATA:-./data}/prometheus
+mkdir -m 777 ${CONTAINER_DATA:-./data}/grafana
+mkdir -m 777 ${CONTAINER_DATA:-./data}/loki
+mkdir -m 777 ${CONTAINER_DATA:-./data}/tempo
